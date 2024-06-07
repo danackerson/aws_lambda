392b3be3fafc668cba9b9e752964560dc9bdf38e
Federico <federicotrotta2@gmail.com>
2024-06-07 14:05:10 +0200

    Finalized code and added readme

--- function/function.py
+++ function/function.py
@@ -1,59 +1,65 @@
 import boto3
-cloudwatch = boto3.client('cloudwatch')
 
-response = cloudwatch.put_metric_data(
-    Namespace='MyApp',
-    MetricData=[
-        {
-            'MetricName': 'CPUUsage',
-            'Dimensions': [
-                {
-                    'Name': 'FunctionName',
-                    'Value': 'my_lambda_function'
-                },
-            ],
-            'Value': 70.0,
-            'Unit': 'Percent'
-        },
-    ]
-)
+def lambda_handler(event, context):
+    cloudwatch = boto3.client('cloudwatch')
 
-response = cloudwatch.put_metric_alarm(
-    AlarmName='HighCPUUsageAlarm',
-    MetricName='CPUUsage',
-    Namespace='MyApp',
-    Statistic='Average',
-    Period=300,
-    EvaluationPeriods=1,
-    Threshold=75.0,
-    ComparisonOperator='GreaterThanThreshold',
-    AlarmActions=[
-        'arn:aws:autoscaling:us-west-2:123456789012:scalingPolicy:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx:autoScalingGroupName/my-asg:policyName/MyScalingPolicy'
-    ]
-)
+    response = cloudwatch.put_metric_data(
+        Namespace='MyApp',
+        MetricData=[
+            {
+                'MetricName': 'CPUUsage',
+                'Dimensions': [
+                    {
+                        'Name': 'FunctionName',
+                        'Value': 'my_lambda_function'
+                    },
+                ],
+                'Value': 70.0,
+                'Unit': 'Percent'
+            },
+        ]
+    )
 
-appscaling = boto3.client('application-autoscaling')
+    response = cloudwatch.put_metric_alarm(
+        AlarmName='HighCPUUsageAlarm',
+        MetricName='CPUUsage',
+        Namespace='MyApp',
+        Statistic='Average',
+        Period=300,
+        EvaluationPeriods=1,
+        Threshold=75.0,
+        ComparisonOperator='GreaterThanThreshold',
+        AlarmActions=[
+            'arn:aws:autoscaling:us-west-2:123456789012:scalingPolicy:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx:autoScalingGroupName/my-asg:policyName/MyScalingPolicy'
+        ]
+    )
 
-response = appscaling.register_scalable_target(
-    ServiceNamespace='lambda',
-    ResourceId='function:my_lambda_function',
-    ScalableDimension='lambda:function:ProvisionedConcurrency',
-    MinCapacity=1,
-    MaxCapacity=10
-)
+    appscaling = boto3.client('application-autoscaling')
 
-response = appscaling.put_scaling_policy(
-    PolicyName='MyScalingPolicy',
-    ServiceNamespace='lambda',
-    ResourceId='function:my_lambda_function',
-    ScalableDimension='lambda:function:ProvisionedConcurrency',
-    PolicyType='TargetTrackingScaling',
-    TargetTrackingScalingPolicyConfiguration={
-        'TargetValue': 75.0,
-        'PredefinedMetricSpecification': {
-            'PredefinedMetricType': 'LambdaProvisionedConcurrencyUtilization'
-        },
-        'ScaleOutCooldown': 60,
-        'ScaleInCooldown': 60
+    response = appscaling.register_scalable_target(
+        ServiceNamespace='lambda',
+        ResourceId='function:my_lambda_function',
+        ScalableDimension='lambda:function:ProvisionedConcurrency',
+        MinCapacity=1,
+        MaxCapacity=10
+    )
+
+    response = appscaling.put_scaling_policy(
+        PolicyName='MyScalingPolicy',
+        ServiceNamespace='lambda',
+        ResourceId='function:my_lambda_function',
+        ScalableDimension='lambda:function:ProvisionedConcurrency',
+        PolicyType='TargetTrackingScaling',
+        TargetTrackingScalingPolicyConfiguration={
+            'TargetValue': 75.0,
+            'PredefinedMetricSpecification': {
+                'PredefinedMetricType': 'LambdaProvisionedConcurrencyUtilization'
+            },
+            'ScaleOutCooldown': 60,
+            'ScaleInCooldown': 60
+        }
+    )
+    return {
+        'statusCode': 200,
+        'body': 'Metrics and alarms configured successfully.'
     }
-)
